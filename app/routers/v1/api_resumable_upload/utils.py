325de237120065331c3d2808d26245e8c5b5adab
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2026-01-06 18:26:35 +0100

    IEBH-462: Fix issue with missing multipart upload parts (#20)

--- app/routers/v1/api_resumable_upload/utils.py
+++ app/routers/v1/api_resumable_upload/utils.py
@@ -4,13 +4,26 @@
 # Version 3.0 (the "License") available at https://www.gnu.org/licenses/agpl-3.0.en.html.
 # You may not use this file except in compliance with the License.
 
+import botocore.exceptions
+
+from app.logger import logger
 from app.models.models_resumable_upload import ObjectInfo
 
 
 async def get_chunks_info(boto3_client, bucket, object_infos: list[ObjectInfo]) -> list[dict]:
     result = []
     for obj_info in object_infos:
-        s3_parts_res = await boto3_client.list_chunks(bucket, obj_info.object_path, obj_info.resumable_id)
+        try:
+            s3_parts_res = await boto3_client.list_chunks(bucket, obj_info.object_path, obj_info.resumable_id)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchUpload':
+                logger.warning(
+                    f'Upload for object "{obj_info.object_path}" '
+                    f'with upload ID "{obj_info.resumable_id}" does not exist.'
+                )
+                continue
+            raise
+
         s3_parts_info = {x.get('PartNumber'): x.get('ETag').replace("\"", '') for x in s3_parts_res.get('Parts', [])}
 
         result.append(
--- tests/routers/v1/api_resumable_upload/test_resumable.py
+++ tests/routers/v1/api_resumable_upload/test_resumable.py
@@ -4,11 +4,12 @@
 # Version 3.0 (the "License") available at https://www.gnu.org/licenses/agpl-3.0.en.html.
 # You may not use this file except in compliance with the License.
 
-import pytest
+import botocore.exceptions
+from common.object_storage_adaptor.boto3_client import Boto3Client
 
+from app.models.models_resumable_upload import ObjectInfo
 from app.routers.exceptions import NotFound
-
-pytestmark = pytest.mark.asyncio
+from app.routers.v1.api_resumable_upload.utils import get_chunks_info
 
 
 async def test_resumable_success_return_200(test_async_client, mocker):
@@ -29,7 +30,6 @@ async def test_resumable_success_return_200(test_async_client, mocker):
 
 
 async def test_resumable_success_return_400(test_async_client, mocker):
-
     m = mocker.patch('app.routers.v1.api_resumable_upload.api_resumable_upload.get_chunks_info', return_value=['test'])
     m.side_effect = Exception
 
@@ -42,3 +42,21 @@ async def test_resumable_success_return_400(test_async_client, mocker):
         pass
     except Exception:
         AssertionError()
+
+
+async def test_get_chunks_info_skips_chunks_that_do_not_exist(mocker, fake):
+    object_infos = [
+        ObjectInfo(object_path=fake.file_path(depth=3), resumable_id=fake.pystr(min_chars=50, max_chars=50))
+        for _ in range(2)
+    ]
+
+    boto3_client = Boto3Client('', '', '')
+    boto3_client.list_chunks = mocker.AsyncMock(
+        side_effect=[{'Parts': []}, botocore.exceptions.ClientError({'Error': {'Code': 'NoSuchUpload'}}, 'ListParts')]
+    )
+
+    chunks_info = await get_chunks_info(boto3_client, 'test-bucket', object_infos)
+
+    assert len(chunks_info) == 1
+    assert chunks_info[0]['object_path'] == object_infos[0].object_path
+    assert chunks_info[0]['resumable_id'] == object_infos[0].resumable_id
